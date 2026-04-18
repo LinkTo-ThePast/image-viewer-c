c6e630da9b8da84d11658c9708ca50eee69d1a65
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-04-18 15:02:31 -0600

    chore(image viewer): adding sdl window

--- ImageViewerDev/imageViewer.c
+++ ImageViewerDev/imageViewer.c
@@ -3,20 +3,10 @@
 #include<SDL.h>
 
 int main() {
-	printf("Welcome to the Image Viewer project! :) ");
-	// initialize window & renderer
-	static SDL_Window* window;
-	static SDL_Renderer* renderer;
+	printf("Image viewer project.");
+	
+	// create a window
+	SDL_Window* window = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 200, NULL);
 
-	window = SDL_CreateWindow("Image viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		600, 900, 0);
-
-	renderer = SDL_CreateRenderer(window, -1, 0);
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-
-	SDL_Delay(6000);
-	SDL_Quit();
 	return 0;
 }
