f332da4782ffea055c6a1f9bc54628ed555c6248
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-05-01 22:38:11 -0600

    chore: adding mapping rgb

--- ImageViewerDev/imageViewer.c
+++ ImageViewerDev/imageViewer.c
@@ -6,7 +6,28 @@ int main() {
 	printf("Image viewer project.");
 	
 	// create a window
-	SDL_Window* window = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 200, NULL);
+	SDL_Window *window = SDL_CreateWindow("Image Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 600, NULL);
+
+	// access to the surface´s window
+	SDL_Surface *currentSurface = SDL_GetWindowSurface(window);
+
+	// pixel format
+	Uint8 r, g, b;
+	r = 0xFF;
+	g = b = 0;
+	
+	// map RGB
+	Uint32 color = SDL_MapRGB(currentSurface->format, r, g, b);
+	
+	// establish the surface as a rectangle to fill
+	SDL_FillRect(currentSurface, NULL, color);
+	// update surface to track new changes
+	SDL_UpdateWindowSurface(window);
+	
+	
+	// adding delay to window so it does not close instantly
+	SDL_Delay(4000);
+	
 
 	return 0;
 }
